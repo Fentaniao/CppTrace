73298aec92a1eda43a3b67c8d218e0a723099bba
望舒 <62752455+Fentaniao@users.noreply.github.com>
2022-01-14 23:02:51 +0800

    Update

--- PowerTrace.h
+++ PowerTrace.h
@@ -9,12 +9,16 @@
  * <p>Copy this .h file to your project directory, and include <b><code>#include "PowerTrace.h"</code></b> in your target .cpp file.
  * <h2>Copyright</h2>
  * @author      Fentaniao
- * @version       2021/10/20
+ * @version       2021/10/21
  */
 
 // TODO 可以考虑使用修饰符进行控制，如"N2D描述S"表示输出traceNum为2，description为描述，S表示储存为日志
 
 #include <iostream>
+#include <cstdio>
+#include <list>
+#include <ctime>
+#include <windows.h>
 
 using namespace std;
 
@@ -23,7 +27,11 @@ using namespace std;
  * <p>trace customize variable
  */
 
+// TODO 完善自定义内容
 //#define varName(x) #x
+//__FILE__
+//__LINE__
+//__FUNCTION__
 
 
 /**
@@ -35,21 +43,28 @@ using namespace std;
 #define TRACE_1(v) Trace(v, #v, __LINE__, __FUNCTION__)
 #define TRACE_2(v, opt1) Trace(v, #v, __LINE__, __FUNCTION__, opt1)
 #define TRACE_3(v, opt1, opt2) Trace(v, #v, __LINE__, __FUNCTION__, opt1, opt2)
+#define TRACE_4(v, opt1, opt2, opt3) Trace(v, #v, __LINE__, __FUNCTION__, opt1, opt2, opt3)
+#define TRACE_5(v, opt1, opt2, opt3, opt4) Trace(v, #v, __LINE__, __FUNCTION__, opt1, opt2, opt3, opt4)
+#define TRACE_6(v, opt1, opt2, opt3, opt4, opt5) Trace(v, #v, __LINE__, __FUNCTION__, opt1, opt2, opt3, opt4, opt5)
+#define TRACE_7(v, opt1, opt2, opt3, opt4, opt5, opt6) Trace(v, #v, __LINE__, __FUNCTION__, opt1, opt2, opt3, opt4, opt5, opt6)
+#define TRACE_8(v, opt1, opt2, opt3, opt4, opt5, opt6, opt7) Trace(v, #v, __LINE__, __FUNCTION__, opt1, opt2, opt3, opt4, opt5, opt6, opt7)
 
 //封装
-#define TRACE_NARG(...) TRACE_ARG_N(__VA_ARGS__, 4, 3, 2, 1, 0)
-#define TRACE_ARG_N(_1, _2, _3, _4, n, ...) n
+#define TRACE_NARG(...) TRACE_ARG_N(__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define TRACE_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, n, ...) n
 #define TRACE_NC(f, ...) f(__VA_ARGS__)
 #define TRACE_NB(nargs, ...) TRACE_NC(TRACE_ ## nargs, __VA_ARGS__)
 #define TRACE_NA(nargs, ...) TRACE_NB(nargs, __VA_ARGS__)
 #define trace(...) TRACE_NA(TRACE_NARG(__VA_ARGS__), __VA_ARGS__)
 
+//函数
 template<typename T>
 void
-Trace(const T var, const string &varName, const int lineNum, const string &function, const int traceNum = -1,
+Trace(const T var, const string &varName, const int lineNum, const string &function,
+      const list<int> &traceNum = list<int>{},
       const string &description = "") {
     //    变量名
-    cout << "TRACE[Var=" << varName << "] ";
+    cout << "TRACE [Var=" << varName << "] ";
 
     //    变量属性：类型
     cout << "[Type=" << typeid(var).name() << "] ";
@@ -60,15 +75,20 @@ Trace(const T var, const string &varName, const int lineNum, const string &funct
     //    标志：函数名，行号，循环参数
     cout << "[Fun=" << function << ", ";
     cout << "Line=" << lineNum;
-    if (traceNum != -1)
-        cout << ", Cycle=" << traceNum;
+    if (!traceNum.empty()) {
+        cout << ", Cycle=(";
+        for (int it: traceNum) {
+            cout << it << " ";
+        }
+        cout << "\b)";
+    }
     cout << "] ";
 
     //    描述
     if (!description.empty())
         cout << "[Desc: " << description << "] ";
 
-    cout << endl;
+    cout << "\b" << endl;
 }
 
 
@@ -81,19 +101,25 @@ Trace(const T var, const string &varName, const int lineNum, const string &funct
 #define TRACEARR_1(v) TraceArr(v, #v, extent<decltype(v)>::value, __LINE__, __FUNCTION__)
 #define TRACEARR_2(v, opt1) TraceArr(v, #v, extent<decltype(v)>::value, __LINE__, __FUNCTION__, opt1)
 #define TRACEARR_3(v, opt1, opt2) TraceArr(v, #v, extent<decltype(v)>::value, __LINE__, __FUNCTION__, opt1, opt2)
+#define TRACEARR_4(v, opt1, opt2, opt3) TraceArr(v, #v, extent<decltype(v)>::value, __LINE__, __FUNCTION__, opt1, opt2, opt3)
+#define TRACEARR_5(v, opt1, opt2, opt3, opt4) TraceArr(v, #v, extent<decltype(v)>::value, __LINE__, __FUNCTION__, opt1, opt2, opt3, opt4)
+#define TRACEARR_6(v, opt1, opt2, opt3, opt4, opt5) TraceArr(v, #v, extent<decltype(v)>::value, __LINE__, __FUNCTION__, opt1, opt2, opt3, opt4, opt5)
+#define TRACEARR_7(v, opt1, opt2, opt3, opt4, opt5, opt6) TraceArr(v, #v, extent<decltype(v)>::value, __LINE__, __FUNCTION__, opt1, opt2, opt3, opt4, opt5, opt6)
+#define TRACEARR_8(v, opt1, opt2, opt3, opt4, opt5, opt6, opt7) TraceArr(v, #v, extent<decltype(v)>::value, __LINE__, __FUNCTION__, opt1, opt2, opt3, opt4, opt5, opt6, opt7)
 
 //封装
-#define TRACEARR_NARG(...) TRACEARR_ARG_N(__VA_ARGS__, 4, 3, 2, 1, 0)
-#define TRACEARR_ARG_N(_1, _2, _3, _4, n, ...) n
+#define TRACEARR_NARG(...) TRACEARR_ARG_N(__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define TRACEARR_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, n, ...) n
 #define TRACEARR_NC(f, ...) f(__VA_ARGS__)
 #define TRACEARR_NB(nargs, ...) TRACEARR_NC(TRACEARR_ ## nargs, __VA_ARGS__)
 #define TRACEARR_NA(nargs, ...) TRACEARR_NB(nargs, __VA_ARGS__)
 #define traceArr(...) TRACEARR_NA(TRACEARR_NARG(__VA_ARGS__), __VA_ARGS__)
 
+//函数
 template<typename T>
 void
 TraceArr(const T var, const string &varName, const int &maxLength, const int lineNum, const string &function,
-         const int traceNum = -1,
+         const list<int> &traceNum = list<int>{},
          const string &description = "") {
     int i;
     //    参数计算
@@ -101,30 +127,70 @@ TraceArr(const T var, const string &varName, const int &maxLength, const int lin
     while (var[++nonEmptyLength] != '\0');
 
     //    变量名
-    cout << "TRACE[Array=" << varName << "] ";
+    cout << "TRACE [Array=" << varName << "] ";
 
     //    变量属性：类型，长度
     cout << "[Type=" << typeid(var[0]).name() << ", "
          << "Len=" << nonEmptyLength << "/" << maxLength << "] ";
 
     //    值
-    cout << "[Set={";
+    cout << "[Array={";
     for (i = 0; i < nonEmptyLength; i++)
         cout << var[i] << ", ";
-    cout << "}] ";
+    cout << "\b\b}] ";
 
     //    标志：函数名，行号，循环参数
     cout << "[Fun=" << function << ", ";
     cout << "Line=" << lineNum;
-    if (traceNum != -1)
-        cout << ", Cycle=" << traceNum;
+    if (!traceNum.empty()) {
+        cout << ", Cycle=(";
+        for (int it: traceNum) {
+            cout << it << " ";
+        }
+        cout << "\b)";
+    }
     cout << "] ";
 
     //    描述
     if (!description.empty())
         cout << "[Desc: " << description << "] ";
 
-    cout << endl;
+    cout << "\b" << endl;
 }
+
+
+//获取时间
+string GetTime() {
+    SYSTEMTIME sys;
+    GetLocalTime(&sys);
+    char tmp[64] = {'\0'};
+    sprintf(tmp, "%4d-%02d-%02d %02d-%02d-%02d", sys.wYear, sys.wMonth, sys.wDay, sys.wHour, sys.wMinute, sys.wSecond);
+    return tmp;
+}
+
+
+//将标准输入流重定向到文件
+void InRedirect2File(const string &fileName = "main.in") {
+    int stateVar_long = sizeof(fileName);
+    char *path = new char[stateVar_long];
+    strcpy(path, fileName.c_str());            //需要#include<cstring>，c_str()返回一个临时指针赋给strXk；
+    freopen(path, "w", stdout);
+}
+
+
+//将标准输出重定向到文件
+void OutRedirect2File(const string &name = "Time") {
+    string logName;
+    if (name == "Time")
+        logName = GetTime() + ".log";
+    else
+        logName = name + ".log";
+
+    int stateVar_long = sizeof(logName);
+    char *path = new char[stateVar_long];
+    strcpy(path, logName.c_str());            //需要#include<cstring>，c_str()返回一个临时指针赋给strXk；
+    freopen(path, "w", stdout);
+}
+
 
 #endif //POWERTRACE_H
--- main.cpp
+++ main.cpp
@@ -1,38 +1,56 @@
 #include <iostream>
+#include "string"
 #include "PowerTrace.h"
 
 using namespace std;
 
 int main() {
+    OutRedirect2File();
     cout << "Hello, World!" << endl;
     cout << "This is a program designed to preview the effect of our library" << endl;
 
-    cout<<">> Trace simple variable: "<< endl;
+    cout << ">> Trace among the file: " << endl;
 
-    cout<<">> Trace array variable: "<< endl;
 
-    cout<<">> Trace in the cycle: "<<endl;
+    cout << ">> Trace simple variable: " << endl;
 
-    cout<<">> Trace among the file: "<<endl;
+    int int_var = 3;
+    double double_var = 3.14;
+    char char_var = 'P';
+    string string_var = "PI";
 
-    cout<<">> Print to the log file: "<<endl;
+    trace(int_var);
+    trace(double_var);
+    trace(char_var);
+    trace(string_var);
 
+
+    cout << ">> Trace array variable: " << endl;
+    int int_arr[60] = {2, 5, 4, 9};
+    double double_arr[60] = {3.14, 6.28, 12.56};
+    char char_arr[60] = {'P', 'I', 'p'};
+
+    traceArr(int_arr);
+    traceArr(double_arr);
+    traceArr(char_arr);
+
+    cout << ">> Trace in the cycle: " << endl;
     int i;
     int j;
 
-    int indexV1 = 3;
-    int weight[58] = {2, 5, 4, 9};
+    for (i = 0; i < 3; ++i)
+        for (j = 0; j < 3; ++j) {
+            trace(int_var, { i });
+            traceArr(int_arr, { i, j });
+        }
 
+    cout << ">> Add description with variable: " << endl;
+    trace(int_var, { i }, "Description");
+    traceArr(int_arr, { i, j }, "Description");
 
-    for (i = 0; i < 3; ++i) {
-        trace(indexV1);
-        trace(indexV1, i);
-        trace(indexV1, i, "23");
-        traceArr(weight);
-        traceArr(weight, i);
-        traceArr(weight, i, "555");
-    }
+    cout << ">> Print to the log file: " << endl;
 
 
     return 0;
 }
+
